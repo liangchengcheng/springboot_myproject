05de8980d89f871c4751a48b58ff407d6c1215b5
liangchengcheng <1038127753@qq.com>
2017-07-13 07:33:00 +0800

    add config class file
    add config class file

--- src/main/java/com/lcc/myproject/config/RabbitMQConfigration.java
+++ src/main/java/com/lcc/myproject/config/RabbitMQConfigration.java
@@ -1,7 +1,65 @@
 package com.lcc.myproject.config;
 
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
 /**
  * Created by liangchengcheng on 2017/7/11.
  */
-public class RabbitMQConfigration {
+@Configuration
+public class RabbitMQConfigration implements RabbitListenerConfigurer{
+
+
+    @Bean
+    public Queue helloQueue(){
+        return new Queue("hello");
+    }
+
+    @Bean
+    public RabbitMessagingTemplate rabbitMessagingTemplate(RabbitTemplate rabbitTemplate) {
+        RabbitMessagingTemplate rabbitMessagingTemplate = new RabbitMessagingTemplate();
+        rabbitMessagingTemplate.setMessageConverter(jackson2Converter());
+        rabbitMessagingTemplate.setRabbitTemplate(rabbitTemplate);
+        return rabbitMessagingTemplate;
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter jackson2Converter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        return converter;
+    }
+
+    //默认  消息  处理  方法  工厂
+    @Bean
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(new MappingJackson2MessageConverter());
+        return factory;
+    }
+
+    //简单  rab  监听 容器 工厂
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        // factory.setPrefetchCount(5);
+        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
+    }
 }
